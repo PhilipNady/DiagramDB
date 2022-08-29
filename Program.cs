24a7b90867b2d99702efa3720076894f0008b175
Philip Gemian <philip.gemian@performly.com>
2022-08-29 07:35:00 +0200

    aa

--- Program.cs
+++ Program.cs
@@ -17,6 +17,7 @@ namespace DiagramDB
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
         }
     }
 }
